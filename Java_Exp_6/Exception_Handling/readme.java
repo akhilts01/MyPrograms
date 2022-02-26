ef4e488f612dd401e1223f641484b9fdc0075269
akhilts01 <98466798+akhilts01@users.noreply.github.com>
2022-02-26 19:45:10 +0530

    Create readme.java

---
+++ Java_Exp_6/Exception_Handling/readme.java
@@ -0,0 +1,7 @@
+Aim: To Familiarize with exception handling mechanism in java
+1. Write a program that demonstrates exception handling mechanism.
+(a) Arithmetic exception
+(b) Array out of index exception
+(c ) File not found exception
+2. Write program that creates a user defined exception when the balance in an account is less than
+500
