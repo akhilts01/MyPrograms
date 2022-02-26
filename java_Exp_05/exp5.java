1a0742c3cb65381963a90509a2f38de707882274
akhilts01 <98466798+akhilts01@users.noreply.github.com>
2022-02-26 19:33:28 +0530

    Create exp5.java

---
+++ java_Exp_05/exp5.java
@@ -0,0 +1,9 @@
+Aim: To familiarize file operations in Java
+1. Write a Java program that reads a file and displays the file on the screen, with a line number 
+before each line by handling all file related exceptions.
+2. Write a Java program that displays the number of characters, lines and words in a text file.
+3. Write a java program that writes your name and address into a file with help of file writer 
+class and read the contents from file and display in the console using appropriate reader 
+class.
+4. Write a Java program that reads a line of integers from a file, and then displays each integer, 
+and the sum of all the integers (Use String Tokenizer class of java.util).
