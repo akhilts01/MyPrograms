d3d1f0131e62d72dad9fbb72ef74217f1b4e58f5
akhilts01 <98466798+akhilts01@users.noreply.github.com>
2022-02-26 19:48:06 +0530

    Create readme.java

---
+++ Java_Exp_7/readme.java
@@ -0,0 +1,10 @@
+Aim : To familiarize with multithreading in java
+1. Write a Java program that implements a multi-threaded program which has three threads. First
+thread generates a random integer every 1 second. If the value is even, second thread computes the
+square of the number and prints. If the value is odd the third thread will print the value of cube of
+the number.
+2. Write a Java program that simulate a banking environment using all the cases thread
+synchronization concept( synchronisation method, synchronization block, static synchronization,
+inter-process communication.
+3. Write a Java program to create two threads: One for displaying all odd number between 1 and
+100 and second thread for displaying all even numbers between 1 and 100.
